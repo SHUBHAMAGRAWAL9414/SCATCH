2f7bb0a23b4dedf7df6977f9d6017cb992c09294
Shubham Agrawal <agarwalshubham924@gmail.com>
2024-07-23 16:59:53 +0530

    project in processing..

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 const express = require("express");
-
 const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const helmet = require("helmet");
+const db = require("./config/mongoose-connection");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
+app.set("view engine", "ejs");
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send("Sorry, we cannot find that!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/ownersRouter.js
@@ -0,0 +1,9 @@
+// routes/ownersRouter.js
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("hey it's working");
+});
+
+module.exports = router;
